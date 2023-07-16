b6516498dd749d311af4503b7a4c51193c119bd0
omeluuk <luuk@sterke.net>
2023-07-16 13:00:12 +0200

    Add logger and init

---
+++ SimpleChatApp/SimpleChatApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SimpleChatApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private static void Init()
+        {
+            [DllImport("kernel32.dll")]
+            static extern IntPtr GetConsoleWindow();
+
+            [DllImport("user32.dll")]
+            static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+            IntPtr ptr = GetConsoleWindow();
+            MoveWindow(ptr, 10, 10, 800, 600, true);
+
+            Console.SetWindowSize(200, 70);
+
+            Logger.Verbose = false;
+        }
+
+        static void Main(string[] args)
+        {
+            Init();
+            Logger.Log("Hello world!");
+        }
+    }
+}
---
+++ SimpleChatApp/SimpleChatApp/utils/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public static class Logger
+{
+    public static bool Verbose = false;
+    public static void Log(string message,
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string sourceFilePath = "",
+        [CallerLineNumber] int sourceLineNumber = 0)
+	{
+        if (Verbose)
+        {
+            Console.WriteLine($"[Caller: {memberName}, File path: {sourceFilePath}, Line number: {sourceLineNumber}]");
+        }
+		Console.WriteLine($"{DateTime.Now} {message}");
+	}
+}
